1ffed561d46a9fb1c2e12ce95a9099ff3a3bf998
jimmyma <jimmyma@croxlink.com>
2018-10-05 18:23:48 +0800

    init flask line bot demo

---
+++ README.md
@@ -0,0 +1,5 @@
+
+[Heroku部署參考網站](https://github.com/twtrubiks/Deploying-Flask-To-Heroku)
+
+推送到 Heroku
+git push heroku master
---
+++ main.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('channel_access_token')
+handler = WebhookHandler('channel_secret')
+
+@app.route("/", methods=['GET'])
+def welcome():
+    return 'Welcom to my web'
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=u'功能開發中，您的訊息為-%s' % event.message.text)
+	)
+
+if __name__ == "__main__":
+    app.run()
