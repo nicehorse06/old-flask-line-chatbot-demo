0827ca8aa8c99fba97399ba7d6d226adc7ad2fe3
jimmyma <jimmyma@croxlink.com>
2018-12-14 17:17:35 +0800

    update star war content

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
+本專案為 line python(Flask) bot 的 demo
 
-[Heroku部署參考網站](https://github.com/twtrubiks/Deploying-Flask-To-Heroku)
+## Line 資料參考網站
+[Line console message API](https://developers.line.me/en/)
 
-推送到 Heroku
-git push heroku master
+[Line python bot sdk](https://github.com/line/line-bot-sdk-python)
+
+
+## line bot 流程
+1. 執行 Flask
+2. 執行 ngrok
+3. 把 ngrok https 網址貼到 Line console
+
+## Flask
+
+* 執行flask `python main.py`
+
+* [Flask官網](http://flask.pocoo.org/)
+
+
+## ngrok 
+
+ngrok 把本地server給外部連
+```
+./ngrok http 5000
+```
+
+## Heroku
+
+* [Heroku部署參考網站](https://github.com/twtrubiks/Deploying-Flask-To-Heroku)
+
+* 推送到 Heroku 執行 `git push heroku master`
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import requests
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -16,9 +18,17 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('channel_access_token')
 handler = WebhookHandler('channel_secret')
 
+# simple Flask demo
 @app.route("/", methods=['GET'])
 def welcome():
     return 'Welcom to my web'
+
+# simple Flask demo
+@app.route('/add/<int:num1>/<int:num2>')
+def show_post(num1, num2):
+    # show the post with the given id, the id is an integer
+    sum =  num1 + num2
+    return 'Sum: ％d + &d = %d' % (num1, num2, sum)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -40,10 +50,22 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if event.message.text == 'hello':
+        reply = 'hello, dude :)'
+    elif 'church' in event.message.text:
+        reply = u'你的輸入中有 "church"'
+    elif 'star war' in event.message.text:
+        r = requests.get('http://docs.python-requests.org/en/master/')
+        this_data = r.json()
+        
+        reply = u'名字:%s\n 身高:%s cm\n 體重:%s kg\n 生日:%s' % (this_data["name"], this_data["height"], this_data["mass"], this_data["birth_year"])
+    else:
+        reply = u'功能開發中，您的訊息為-%s' % event.message.text
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=u'功能開發中，您的訊息為-%s' % event.message.text)
-	)
+        TextSendMessage(text=reply))
 
+# 有這行才能直接執行 python main.py
 if __name__ == "__main__":
     app.run()
